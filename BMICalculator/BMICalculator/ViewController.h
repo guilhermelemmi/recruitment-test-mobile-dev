5f27149335dc3d7b8539ade81df372880de57788
Teste IOS <TesteIOS@tecnonutri-mac-mini.local>
2015-11-19 09:35:27 -0200

    Initial commit for the new Xcode Project, still without any modifications

---
+++ BMICalculator/BMICalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMICalculator
+//
+//  Created by Teste IOS on 11/19/15.
+//  Copyright © 2015 Teste IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BMICalculator/BMICalculatorTests/BMICalculatorTests.m
@@ -0,0 +1,39 @@
+//
+//  BMICalculatorTests.m
+//  BMICalculatorTests
+//
+//  Created by Teste IOS on 11/19/15.
+//  Copyright © 2015 Teste IOS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BMICalculatorTests : XCTestCase
+
+@end
+
+@implementation BMICalculatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
